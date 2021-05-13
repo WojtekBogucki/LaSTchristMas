0bfdb817720252a6034cdfd943de9358afa4df1c
WojtekBogucki <w.bogucki@student.mini.pw.edu.pl>
2021-05-13 20:38:20 +0200

    Testing architecture

--- src/WB2.py
+++ src/WB2.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import random
 import tensorflow as tf
-from src.utils import log_specgram, pad_audio, chop_audio, label_transform, list_wavs_fname, plot_confusion_matrix
+from src.utils import log_specgram, pad_audio, chop_audio, label_transform, list_wavs_fname, plot_confusion_matrix, visualize
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Dense, SimpleRNN, LSTM, Bidirectional, TimeDistributed, Conv1D, ZeroPadding1D, GRU
 from tensorflow.keras.layers import Lambda, Input, Dropout, Masking, BatchNormalization, Activation
@@ -34,22 +34,22 @@ with open("data/y_val.pickle", "rb") as f:
 
 classes = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go', 'unknown', 'silence']
 
-def cnn_lstm2(input_dim, output_dim, dropout=0.2):
+def cnn_lstm2(input_dim, output_dim, dropout=0.4, seed=420):
     # Input data type
-    reset_random_seeds(420)
+    reset_random_seeds(seed)
     dtype = 'float32'
     model = Sequential([
-        Conv1D(filters=512, kernel_size=15, strides=4, input_shape=input_dim, dtype=dtype),
+        Conv1D(filters=512, kernel_size=10, strides=4, input_shape=input_dim, dtype=dtype),
         Activation('relu'),
         BatchNormalization(),
         Dropout(dropout),
-        Conv1D(filters=256, kernel_size=15, strides=4),
+        Conv1D(filters=256, kernel_size=10, strides=4),
         Activation('relu'),
         BatchNormalization(),
         Dropout(dropout),
-        LSTM(512, activation='tanh', return_sequences=True, recurrent_activation='sigmoid', dropout=dropout),
-        LSTM(512, activation='tanh', return_sequences=False, recurrent_activation='sigmoid', dropout=dropout),
-        Dense(units=128, activation='relu'),
+        LSTM(128, activation='tanh', return_sequences=True, recurrent_activation='sigmoid', dropout=dropout),
+        LSTM(128, activation='tanh', return_sequences=False, recurrent_activation='sigmoid', dropout=dropout),
+        Dense(units=64, activation='relu'),
         Dropout(dropout),
         Dense(units=output_dim, activation='softmax')
     ])
@@ -58,22 +58,46 @@ def cnn_lstm2(input_dim, output_dim, dropout=0.2):
 
 input_dim = (99, 161)
 n_classes = len(classes)
-K.clear_session()
-model2 = cnn_lstm2(input_dim, n_classes)
-model2.summary()
-
 adam = Adam(lr=1e-4, clipnorm=1.0)
+models3 = []
+histories3 = []
+predictions3 = []
+for seed in [420, 1234, 4567]:
+    K.clear_session()
+    model = cnn_lstm2(input_dim, n_classes, 0.4, seed)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=adam,
+                  metrics=['accuracy'])
+    print("Model  seed: {0}".format(seed))
+    history = model.fit(x_train, y_train,
+                        batch_size=128, epochs=50,
+                        validation_data=(x_val, y_val)
+                        )
+    pred = model.predict(x_val)
+    # plot_confusion_matrix(y_val.argmax(axis=1),pred.argmax(axis=1), normalize=True, classes=classes, filename="model1_drop_{}".format(int(dropout*100)))
+    models3.append(model)
+    histories3.append(history)
+    predictions3.append(pred)
 
-model2.compile(loss='categorical_crossentropy',
-              optimizer=adam,
-              metrics=['accuracy'])
-history2 = model2.fit(x_train, y_train,
-                    batch_size=128, epochs=20,
-                    validation_data=(x_val, y_val)
-                    )
 
-pd.DataFrame(history2.history).plot()
-model2.evaluate(x_val, y_val)
-pred2 = model2.predict(x_val)
+with open("hist2.pickle", "rb") as f:
+    pickle.load(f)
 
-plot_confusion_matrix(y_val.argmax(axis=1),pred2.argmax(axis=1), normalize=True, classes=classes, filename="model2_conf_mat")
+labels = list(np.array([[name + " " +str(i) for i in range(1, 4)] for name in ["dropout=0", "dropout=0.2", "dropout=0.4"]]).flatten())
+visualize(histories, labels, "loss", title="Comparison of loss on training set", filename="model1_drop")
+visualize(histories, labels, "accuracy", title="Comparison of accuracy on training set", filename="model1_drop")
+visualize(histories, labels, "val_loss", title="Comparison of loss on validation set", filename="model1_drop")
+visualize(histories, labels, "val_accuracy", title="Comparison of accuracy on validation set", filename="model1_drop")
+
+losses=[]
+accs=[]
+for model in models:
+    loss, acc = model.evaluate(x_val, y_val)
+    losses.append(loss)
+    accs.append(acc)
+
+stats = pd.DataFrame({"model": ["dropout=0", "dropout=0.2", "dropout=0.4"],
+                      "avg_loss": [np.mean(losses[:3]),np.mean(losses[3:6]),np.mean(losses[6:9])],
+                      "avg_acc": [np.mean(accs[:3]),np.mean(accs[3:6]),np.mean(accs[6:9])]})
+
+stats.to_csv("stats/model1_stats.csv")
--- src/utils.py
+++ src/utils.py
@@ -140,6 +140,29 @@ def visualize(histories, labels=None, type="val_accuracy", filename=None, start_
     else:
         plt.show()
 
+def visualize2(histories, labels=None, type="val_accuracy", filename=None, start_from=0, title=None):
+    fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # cm = plt.get_cmap('gist_rainbow')
+    # NUM_COLORS = len(histories)
+    # ax.set_prop_cycle(color=[cm(1. * i / NUM_COLORS) for i in range(NUM_COLORS)])
+    if not title:
+        title = type
+    if not labels:
+        labels = ["Model {0}".format(j) for j in range(1, len(histories) + 1)]
+    for hist, label in zip(histories, labels):
+        y = np.array(hist[type])
+        plt.plot(range(start_from + 1, len(y) + 1), y[start_from:], label=label)
+        plt.title(title)
+        plt.xlabel("number of epoch")
+        plt.ylabel(type)
+        plt.legend()
+    if filename:
+        plt.savefig("plots/{0}_{1}".format(filename, type))
+        plt.close(fig)
+    else:
+        plt.show()
+
 # https://github.com/yungshun317/keras-rnn-speech-recognizer/blob/master/train_utils.py
 def ctc_lambda_func(args):
     y_pred, labels, input_length, label_length = args
